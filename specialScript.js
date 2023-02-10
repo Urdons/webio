34aff229e858891e6e0fc07ce7c9d65ff0f18181
Urdons <urdons101@gmail.com>
2023-02-09 20:57:38 -0500

    redid specialpage

--- specialScript.js
+++ specialScript.js
@@ -1,38 +1,21 @@
-let dropdownMenuOpen = false;
-let onMobile = false;
-
-let dropdownGroup = document.getElementById("dropdown");
 let dropdownButton = document.getElementById("dropdownButton");
 let dropdownMenu = document.getElementById("dropdownMenu");
 
-if (document.body.offsetWidth / document.body.offsetHeight < 40) {
-	onMobile = true;
-	dropdownMenu.style.top = '3.5em';
-	dropdownMenu.style.width = document.body.offsetWidth + 'px';
-	dropdownMenu.style.left = '-' + dropdownMenu.style.width;
-} else {
-	onMobile = false;
-	dropdownMenu.style.top = '0em';
-	dropdownMenu.style.width = '25em';
-	dropdownMenu.style.left = '-25em';
-}
-
 function dropdown () {
-    if (dropdownMenuOpen) {
-		if (onMobile) { dropdownButton.style.left = 0}
-        dropdownGroup.style.left = '0em';
-		
-        dropdownButton.firstElementChild.src = 'Images/icons/dropdown.svg';
-        dropdownButton.firstElementChild.alt = 'dropdown';
-
-        dropdownMenuOpen = false;
+    if (window.innerWidth / window.innerHeight < 0.9) {
+        dropdownMenu.classList.add('m');
+        dropdownButton.classList.add('m');
     } else {
-		if (onMobile) { dropdownButton.style.left = -(document.body.offsetWidth/2 + dropdownButton.offsetWidth/2) + 'px'}
-        dropdownGroup.style.left = dropdownMenu.offsetWidth + 'px';
+        dropdownMenu.classList.remove('m');
+        dropdownButton.classList.remove('m');
+    }
 
+    dropdownMenu.classList.toggle('active');
+    dropdownButton.classList.toggle('active');
+
+    if (dropdownButton.classList.contains('active')) {
         dropdownButton.firstElementChild.src = 'Images/icons/close.svg'
-        dropdownButton.firstElementChild.alt = 'close';
-
-        dropdownMenuOpen = true;
+    } else {
+        dropdownButton.firstElementChild.src = 'Images/icons/dropdown.svg'
     }
 }
