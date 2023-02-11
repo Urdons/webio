ff4276bf571016a988feb210681b701f066bb40c
Urdons <urdons101@gmail.com>
2023-02-10 19:40:31 -0500

    fixed some scaling issues and new header

--- specialScript.js
+++ specialScript.js
@@ -1,7 +1,6 @@
-let dropdownButton = document.getElementById("dropdownButton");
-let dropdownMenu = document.getElementById("dropdownMenu");
+checkRatio();
 
-function dropdown () {
+function checkRatio () {
     if (window.innerWidth / window.innerHeight < 0.9) {
         dropdownMenu.classList.add('m');
         dropdownButton.classList.add('m');
@@ -9,6 +8,13 @@ function dropdown () {
         dropdownMenu.classList.remove('m');
         dropdownButton.classList.remove('m');
     }
+}
+
+function dropdown () {
+    checkRatio();
+
+    let dropdownButton = document.getElementById("dropdownButton");
+    let dropdownMenu = document.getElementById("dropdownMenu");
 
     dropdownMenu.classList.toggle('active');
     dropdownButton.classList.toggle('active');
